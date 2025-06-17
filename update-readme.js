f37f1a875c27641d545b2100676e146b42129ebf
d-beloved <morayodeji@gmail.com>
2025-06-17 16:27:33 +0100

    update the update-readme script

--- update-readme.js
+++ update-readme.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const { graphql } = require('@octokit/graphql');
+import { readFileSync, writeFileSync } from 'fs';
+import { graphql } from '@octokit/graphql';
 
 const token = process.env.GITHUB_TOKEN;
-const username = 'd-beloved';
+const username = 'd-beloved'; // Your GitHub username
 
 const query = `{
   user(login: "${username}") {
@@ -19,8 +19,9 @@ const query = `{
               color
             }
           }
+          stargazerCount
+          forkCount
           updatedAt
-          openGraphImageUrl
         }
       }
     }
@@ -45,10 +46,6 @@ async function updateReadme() {
     // Grid layout with cards
     let gridLayout = '';
     repos.forEach(repo => {
-      const languages = repo.languages.nodes.map(lang => 
-        getLanguageBadge(lang.name, lang.color)
-      ).join(' ');
-      
       gridLayout += `<a href="${repo.url}">
     <img src="https://github-readme-stats.vercel.app/api/pin/?username=${username}&repo=${repo.name}&theme=radical" />
   </a> `;
@@ -59,51 +56,17 @@ async function updateReadme() {
       }
     });
     
-    // Table layout with more details
-    let tableLayout = '<table>\n';
-    for (let i = 0; i < repos.length; i += 2) {
-      tableLayout += '  <tr>\n';
-      
-      for (let j = i; j < Math.min(i + 2, repos.length); j++) {
-        const repo = repos[j];
-        const languages = repo.languages.nodes.map(lang => 
-          getLanguageBadge(lang.name, lang.color)
-        ).join(' ');
-        
-        tableLayout += `    <td align="center" width="50%">
-      <a href="${repo.url}"><strong>${repo.name}</strong></a>
-      <br />
-      <br />
-      ${languages}
-      <br />
-      <br />
-      <p>${repo.description || 'No description available'}</p>
-    </td>\n`;
-      }
-      
-      // If odd number of repos, add empty cell
-      if (i + 1 >= repos.length && repos.length % 2 === 1) {
-        tableLayout += '    <td width="50%"></td>\n';
-      }
-      
-      tableLayout += '  </tr>\n';
-    }
-    tableLayout += '</table>';
-    
     // Read the current README
-    const readmeContent = fs.readFileSync('README.md', 'utf8');
-    
-    // Choose which layout you prefer - grid or table
-    const chosenLayout = gridLayout; // or tableLayout if you prefer that one
+    const readmeContent = readFileSync('README.md', 'utf8');
     
     // Replace the featured projects section
     const newReadme = readmeContent.replace(
       /<!-- FEATURED_PROJECTS_START -->[\s\S]*?<!-- FEATURED_PROJECTS_END -->/,
-      `<!-- FEATURED_PROJECTS_START -->\n${chosenLayout}\n  <!-- FEATURED_PROJECTS_END -->`
+      `<!-- FEATURED_PROJECTS_START -->\n${gridLayout}\n  <!-- FEATURED_PROJECTS_END -->`
     );
     
     // Write the new README
-    fs.writeFileSync('README.md', newReadme);
+    writeFileSync('README.md', newReadme);
     
     console.log('README updated successfully with featured projects!');
   } catch (error) {
